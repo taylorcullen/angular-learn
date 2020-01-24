2abd2748878d86fd34b8b6179da47b63fbfc7d37
Taylor Cullen <Taylor.Cullen@city-holdings.co.uk>
2020-01-24 15:18:15 +0000

    End of module 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -81,8 +81,6 @@ namespace DatingApp.API.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
             return Ok(new { token = tokenHandler.WriteToken(token)});
-
         }
-
     }
 }
